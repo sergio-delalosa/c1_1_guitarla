07e723ee011bc53c6fc577b4aa89a8e49bc33b90
Sergio <sergio.losa.merino@gmail.com>
2024-09-17 16:04:47 +0200

    Añadido carrito

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,21 @@ function App() {
   const [cart, setCart]= useState([])
 
   function addToCart(item) {
-    setCart(prevVart =>[...prevVart, item])
+    const itemExists = cart.findIndex(guitar => guitar.id === item.id);
+    if (itemExists >= 0) { 
+      const updatedCart=[...cart]
+      updatedCart[itemExists].quantity++
+      setCart(updatedCart)
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item])
+    }
   }
 
   return (
     <>
-     <Header />    
+     <Header 
+      cart={cart}/>    
 
     <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
